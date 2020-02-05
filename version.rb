851086a150134313d5ca401036018bde439992f1
rultor <me@rultor.com>
2020-02-05 20:45:04 +0000

    build number set

--- version.rb
+++ version.rb
@@ -24,8 +24,8 @@ require_relative 'objects/xia'
 
 module Xia
   # Currently released version
-  VERSION = '0.0.0'
+  VERSION = '0.0.12'
 
   # The Git hash of the release
-  RELEASE = '000000'
+  RELEASE = '86a401d'
 end
