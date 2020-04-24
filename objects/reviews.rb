52cfccf7dcd104d0fcbb4ce84589d2e042dcfba0
iakunin <iakunin@users.noreply.github.com>
2020-04-24 18:39:35 +0300

    #106 - Increasing daily limits for bots review submission up to 300

--- objects/reviews.rb
+++ objects/reviews.rb
@@ -79,7 +79,7 @@ class Xia::Reviews
   def quota
     return 1 if @project.author.vip?
     max = 5
-    max = 100 if Xia::Bots.new.is?(@project.author)
+    max = 300 if Xia::Bots.new.is?(@project.author)
     max - @pgsql.exec(
       'SELECT COUNT(*) FROM review WHERE created > NOW() - INTERVAL \'1 DAY\' AND author=$1',
       [@project.author.id]
