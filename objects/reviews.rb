c2cddbc3c6b72a5480e84d1ac05d470f1a7e2405
yegor256 <yegor256@gmail.com>
2020-04-26 10:45:14 +0300

    #121 DESC for reviews

--- objects/reviews.rb
+++ objects/reviews.rb
@@ -96,7 +96,7 @@ class Xia::Reviews
       'JOIN author ON author.id=r.author',
       'WHERE project=$1',
       show_deleted ? '' : ' AND r.deleted IS NULL',
-      'ORDER BY r.created ASC',
+      'ORDER BY r.created DESC',
       'LIMIT $2 OFFSET $3'
     ].join(' ')
     @pgsql.exec(q, [@project.id, limit, offset]).map do |r|
