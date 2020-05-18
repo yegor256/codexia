d6ea366fcc08bba48500fdeb42bdfdcece8293ca
yegor256 <yegor256@gmail.com>
2020-05-18 23:06:46 +0300

    #152 CSV and JSON

--- front/front_karma.rb
+++ front/front_karma.rb
@@ -20,12 +20,38 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+require 'csv'
+require 'json'
+
 get '/karma' do
   page = (params[:page] || '0').strip.to_i
   haml :karma, layout: :layout, locals: merged(
     title: '/karma',
+    page: page,
     recent: the_author.karma.recent(offset: page * 25, limit: 25),
     withdrawals: the_author.withdrawals.recent(limit: 10)
+  )
+end
+
+get '/karma.csv' do
+  content_type 'text/plain'
+  CSV.generate do |csv|
+    the_author.karma.recent(limit: 100_000_000).map do |k|
+      csv << [k[:created].iso8601, k[:points], k[:text]]
+    end
+  end
+end
+
+get '/karma.json' do
+  content_type 'application/json'
+  JSON.pretty_generate(
+    the_author.karma.recent(limit: 100_000_000).map do |k|
+      {
+        created: k[:created].iso8601,
+        points: k[:points],
+        description: k[:text]
+      }
+    end
   )
 end
 
